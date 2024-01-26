d2f53fdc222e86bcf2d9b8046bc8fa444e4bebdc
Manoj Kumar Saini <128309340+ManojKumSaini@users.noreply.github.com>
2024-01-27 01:03:46 +0530

    Update Random number - Game.py

--- Random number - Game.py
+++ Random number - Game.py
@@ -1,40 +1,29 @@
-
 import random
+import time
+Name = input("May I have your name please?: ")
 
-max_digit = input("Write the biggest number by you want to guess: ")
+print(f"Hello {Name}! Welcome to the guessing game. I am going to pick a number between 1 and 100.")
+time.sleep(2)
 
-if int(max_digit) is False:
-    print("Write a valid number above 0.")
-elif int(max_digit) <= 0:
-    print("Write a number above 0.")
-else:
-    max_digit = int(max_digit)
+print("Picking a number...")
+time.sleep(2)
+
+guess = int(input("What is your guess?: "))
+correct_number = random.randint(1,100)
+guesses = 1
+
+while guess != correct_number:
+    guesses += 1
     
-number = random.randint(0,max_digit)
-
-number_of_guess = 0
-
-while number_of_guess < 5:
-    print('Guess a number between 0 and', max_digit,".")
-    guess = input()
-    guess = int(guess)
-    number_of_guess += 1
-    
-    if number == guess:
-        break
-    elif number_of_guess == 5:
-        break
-    else:
-        print("You guessed it wrong!")
-        if guess > number:
-            print("Guessed number is higher than the random number.")
+    if guess > correct_number:
+        if guess - correct_number < 5:
+            guess = int(input("You are going good. Just try a little lower. Have a guess now?: "))
         else:
-            print("Guessed number is lower than the random number.")
-
-
-
-if number == guess:
-    print("Great, You guessed it right!")
-elif number_of_guess >= 5:
-    print("Unfortunately, Your chances are over.")
-
+            guess = int(input("Nope! Please guess a lower number. Try Now: "))
+    if guess < correct_number:
+        if correct_number - guess < 5:
+            guess = int(input("Ah! You are close to the number. Just try a little higher. Have your guess now?: "))
+        else:
+            guess = int(input("Wrong! You need to guess higher. What is your guess?: "))
+    
+print(f"Congrats {Name}!, You guessed it correct. It took {guesses} guesses.")    
